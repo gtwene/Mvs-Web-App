8340d34ea7a3552c742130c5e96ec62372b0fad0
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-14 14:16:30 +0000

    raw data added

---
+++ mvs-movies/src/Banner.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import requests from './requests';
+
+const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    },[]);
+
+    console.log(movie)
+
+    return (
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}
+        > 
+            <div className="banner__contents"> 
+            {/* title */}
+            <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+
+            {/* div with two buttons */}
+            <div className="banner__buttons">
+                <button className="banner__button">Play</button>
+                <button className="banner__button">My List</button>
+            </div>
+
+            {/* discription */}
+            <h1 className="banner_description">
+                {movie?.overview}
+            </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Banner
