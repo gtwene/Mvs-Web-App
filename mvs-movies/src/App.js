8da85ddd46aaf56f58d390ceae52807a93c1a2ff
Kwame <gtwene001@st.ug.edu.gh>
2021-11-09 12:08:18 +0000

    changes

--- mvs-movies/src/App.js
+++ mvs-movies/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner';
-import Nav from './Nav';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-     {/* Nav bar at the top */}
-     <Nav />
+      {/* Nav bar at the top */}
+      <Nav />
 
-     {/* Banner */}
-     <Banner />
-      <Row 
-        title="NETFLIX ORIGINALS" 
-        fetchUrl={requests.fetchNetflixOriginals} 
+      {/* Banner */}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
@@ -25,7 +25,6 @@ function App() {
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-
     </div>
   );
 }
